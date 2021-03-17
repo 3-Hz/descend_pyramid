ae9a238a5e43256d050686cc41ec100c5e8d0b41
3-Hz <edward.h.zhao@gmail.com>
2021-03-17 16:43:59 -0500

    optimized recursive solution

--- pyramidDescent.js
+++ pyramidDescent.js
@@ -4,23 +4,24 @@ const descendPyramid = (input) => {
   // read input file and split by newline
   let contents = fs.readFileSync(input, 'utf-8').split('\n');
   // split first line by space and get target value
-  let target = contents[0].split(' ')[1];
+  let target = Number(contents[0].split(' ')[1]);
   // splice pyramid from contents and map to list
   let pyramid = contents.splice(1).map(level => level.split(','));
 
-  let dfTraverse = (pyramid, target, index, depth, product, path) => {
+  let dfTraverse = (pyramid, target, index = 0, depth = 0, product = 1, path = '') => {
     product *= pyramid[depth][index];
-    //console.log(depth, index, path, product, target);
     if (depth === pyramid.length - 1 && product === target) {
-      console.log(path);
+      return path;
+    }
+    if (product > target) {
       return;
     }
     if (depth < pyramid.length - 1) {
-      return dfTraverse(pyramid, target, index, depth + 1, product, path + 'L') ?? dfTraverse(pyramid, target, index + 1, depth + 1, product, path + 'R');
+      return dfTraverse(pyramid, target, index, depth + 1, product, path + 'L') || dfTraverse(pyramid, target, index + 1, depth + 1, product, path + 'R');
     }
   }
 
-  dfTraverse(pyramid, Number(target), 0, 0, 1, '');
+  console.log(dfTraverse(pyramid, target));
 }
 
 descendPyramid('./sample_input.txt');
